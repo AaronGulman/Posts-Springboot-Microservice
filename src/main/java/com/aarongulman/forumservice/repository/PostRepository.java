55f4c59ae758e71e125005e171335a2b59775e09
Aaron Gulman <enggulman@gmail.com>
2025-04-27 21:23:42 -0400

    Add PostRepository interface extending JpaRepository

--- src/main/java/com/aarongulman/forumservice/repository/PostRepository.java
+++ src/main/java/com/aarongulman/forumservice/repository/PostRepository.java
@@ -1,4 +1,12 @@
 package com.aarongulman.forumservice.repository;
 
-public class PostRepository {
+import com.aarongulman.forumservice.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long> {
+
 }

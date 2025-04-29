b2635c4e8d7a7a8264187261d65baaf90823abb6
Aaron Gulman <enggulman@gmail.com>
2025-04-29 09:02:10 -0400

    Add update,delete and fetch-by-id endpoints to CommentController

--- src/main/java/com/aarongulman/forumservice/controller/CommentController.java
+++ src/main/java/com/aarongulman/forumservice/controller/CommentController.java
@@ -1,5 +1,6 @@
 package com.aarongulman.forumservice.controller;
 
+import com.aarongulman.forumservice.exceptions.CommentNotFoundException;
 import com.aarongulman.forumservice.model.Comment;
 import com.aarongulman.forumservice.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,4 +25,33 @@ public class CommentController {
     public Iterable<Comment> getAllComments(){
         return commentRepository.findAll();
     }
+
+    @PutMapping("/{id}")
+    Comment replaceComment(@RequestBody Comment newComment,@PathVariable Long id){
+        return commentRepository.findById(id)
+                .map(comment -> {
+                    comment.setMessage(newComment.getMessage());
+                    return commentRepository.save(comment);
+                })
+                .orElseGet(()->{
+                    return commentRepository.save(newComment);
+                });
+    }
+
+    @GetMapping("/{id}")
+    Comment one(@PathVariable Long id){
+        return commentRepository.findById(id)
+                .orElseThrow(()->new CommentNotFoundException(id));
+    }
+
+    @DeleteMapping("/id")
+    void deleteComment(@PathVariable Long id){
+        commentRepository.deleteById(id);
+    }
+
+
+
+
+
+
 }

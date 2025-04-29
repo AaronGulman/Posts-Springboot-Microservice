d535ab3cc463a7d733aba4fb527ef1766e8bbea8
Aaron Gulman <enggulman@gmail.com>
2025-04-28 21:20:38 -0400

    Add CommentController with endpoints for adding and retrieving comments

--- src/main/java/com/aarongulman/forumservice/controller/CommentController.java
+++ src/main/java/com/aarongulman/forumservice/controller/CommentController.java
@@ -1,4 +1,27 @@
 package com.aarongulman.forumservice.controller;
 
+import com.aarongulman.forumservice.model.Comment;
+import com.aarongulman.forumservice.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping("/comments")
 public class CommentController {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @PostMapping("/addComment")
+    public String addComment(@RequestBody Comment comment){
+        comment.setCreatedAt(java.time.LocalDateTime.now());
+        commentRepository.save(comment);
+        return "Comment saved";
+    }
+
+    @GetMapping
+    public Iterable<Comment> getAllComments(){
+        return commentRepository.findAll();
+    }
 }
